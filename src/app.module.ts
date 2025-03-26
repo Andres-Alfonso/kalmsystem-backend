6a7e6988a5977dd4666ec79ca1d8399555fd760c
andres-alfonso <falfonso@windowschannel.com>
2025-03-26 08:35:27 -0500

    Se agrega conection a base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // Importar y configurar ConfigModule
+    ConfigModule.forRoot({
+      isGlobal: true, // Disponible en toda la aplicación
+      envFilePath: '.env', // Ruta al archivo .env
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3307,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+    TypeOrmModule.forFeature([ ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
